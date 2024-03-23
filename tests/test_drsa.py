6e0ad88e7c62c747748f8911e0a50af41ba871f8
Rafal Bachorz <rafal@bachorz.eu>
2024-03-23 10:23:58 +0100

    Working classifier test

--- tests/test_drsa.py
+++ tests/test_drsa.py
@@ -1,12 +1,29 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import pytest
+from DRSA import DRSA 
 RANDOM_SEED = 42
 
-def test_classifier():
+@pytest.fixture
+def decision_matrix():
     np.random.seed(RANDOM_SEED)
     n_attr = 3
-    n_alt = 10
+    n_alt = 20
     dm = pd.DataFrame(np.random.sample(size=(n_alt, n_attr)), columns=['attr'+str(item) for item in range(n_attr)])
     target = pd.DataFrame(np.random.randint(0, 2, n_alt), columns=['target'])
     dm = pd.concat([dm, target], axis=1)
-    pass
+    return dm
+
+def test_classifier(decision_matrix):
+    X = decision_matrix.iloc[:, :-1]
+    y = decision_matrix.iloc[:, -1]
+    XX = pd.DataFrame()
+    for x in X:
+        XX[x+"-"] = X[x].copy()
+    X = pd.concat([X, XX], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=5)
+    drsa = DRSA([1, 1, 1, -1, -1, -1,])
+    drsa.fit(X_train.reset_index(drop=True), y_train.reset_index(drop=True))
+    y_pred = drsa.predict(X_test)
+    assert((np.array([0, 1, 0, 0, 0]) == y_pred).sum() == 5)
